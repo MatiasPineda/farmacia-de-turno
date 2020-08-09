3b100b9fdd645b12908b05f1b09125caed6dca2d
MatiasPineda <matias.pinedav@gmail.com>
2020-08-09 01:44:19 -0400

    fixed missed '/' in url

--- farmacias_app/urls.py
+++ farmacias_app/urls.py
@@ -6,7 +6,7 @@ from .views import *
 routers = routers.SimpleRouter()
 
 urlpatterns = [
-    path('localidades', ListRegion.as_view(), name='localidades'),
+    path('localidades/', ListRegion.as_view(), name='localidades'),
     path('localidades/<str:pk>/', DetailComuna.as_view()),
     path('farmaciasturno/', ListFarmaciasTurno.as_view(), name='farmaciasturno'),
     path('farmaciasturno/<int:pk>/', DetailFarmaciasTurno.as_view()),
