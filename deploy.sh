b8fb38e6ff59d60fcf86e6ac34605143a7035392
MatiasPineda <matias.pinedav@gmail.com>
2020-08-09 02:11:44 -0400

    .

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'deploy'
 # git push -f git@github.com:MatiasPineda/MatiasPineda.github.io.git gh-pages
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:MatiasPineda/farmacia-de-turno.git frontend:gh-pages
+git push -f git@github.com:MatiasPineda/farmacia-de-turno.git master:gh-pages
 
 cd -
